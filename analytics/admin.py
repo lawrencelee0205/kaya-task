18bf681b5015dd0403e8d0f91cf4784bb2cb779d
lawrencelee0205 <lawrencelee0205.dev@gmail.com>
2024-12-01 14:35:46 +0800

    KT-3 Create admin panels for models.

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Campaign, AdGroup, AdGroupStats
 
 # Register your models here.
+admin.site.register(Campaign)
+admin.site.register(AdGroup)
+admin.site.register(AdGroupStats)
