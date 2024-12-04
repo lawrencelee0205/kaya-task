727a203554dc8d4fe104a2511dcea0308c33b8b2
lawrencelee0205 <lawrencelee0205.dev@gmail.com>
2024-12-04 20:48:36 +0800

    KT-20 Implement default api rate limit.

--- marketing_api/settings.py
+++ marketing_api/settings.py
@@ -139,4 +139,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "5/min",
+    },
 }
