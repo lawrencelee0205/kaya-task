a0e4c6d9cf5a7286367251eb006dfd8e59c41129
lawrencelee0205 <lawrencelee0205.dev@gmail.com>
2024-12-05 12:29:59 +0800

    KT-26 Refine workflow and create populating data script.

--- dev_start_compose.sh
+++ dev_start_compose.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
-echo "Freezeing requirements..."
+echo "Freezing requirements..."
 pipenv requirements --dev > requirements.txt
 
 echo "Starting docker compose services..."
 docker compose up -d --build
+
+echo "Migrating the database..."
+docker compose exec app python manage.py migrate
+
+echo "Populating the data into tables..."
+python dev_populate_data.py
